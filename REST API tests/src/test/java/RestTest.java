4922986ab69ef2bbec73e9d107deb356ce22e7e8
dev4lex <dev4lex@gmail.com>
2021-11-10 21:34:22 +0300

    add RequestSpecification

--- REST API tests/src/test/java/RestTest.java
+++ REST API tests/src/test/java/RestTest.java
@@ -1,5 +1,9 @@
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+import pojos.CreateUserRequest;
+import pojos.CreateUserResponse;
 import pojos.UserPojo;
 import pojos.UserPojoFull;
 
@@ -13,17 +17,39 @@ import static org.hamcrest.core.IsEqual.equalTo;
 
 public class RestTest {
 
+    private static final RequestSpecification REQ_SPEC =
+            new RequestSpecBuilder()
+                    .setBaseUri("https://reqres.in/api")
+                    .setBasePath("/users")
+                    .setContentType(ContentType.JSON)
+                    .build();
+
     @Test
     public void getUsers(){
         List<UserPojoFull> users = given()
-                .baseUri("https://reqres.in/api")
-                .basePath("/users")
-                .contentType(ContentType.JSON)
+                .spec(REQ_SPEC)
                 .when().get()
                 .then()
                 .statusCode(200)
                 .extract().jsonPath().getList("data", UserPojoFull.class);
 
         assertThat(users).extracting(UserPojoFull::getEmail).contains("george.bluth@reqres.in");
+    }
+    @Test public void createUser(){
+        CreateUserRequest rq = new CreateUserRequest();
+        rq.setName("Alex");
+        rq.setJob("QA Engineer");
+
+        CreateUserResponse rs = given()
+                .spec(REQ_SPEC)
+                .body(rq)
+                .when().post()
+                .then().extract().as(CreateUserResponse.class);
+
+        assertThat(rs)
+                .isNotNull()
+                .extracting(CreateUserResponse::getName)
+                .isEqualTo(rq.getName());
+
     }
 }
