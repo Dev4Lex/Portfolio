be2b3de3e3781ccc51bb27a589ca70c61a6120a0
dev4lex <dev4lex@gmail.com>
2021-11-10 21:34:55 +0300

    add JsonFormat

--- REST API tests/src/test/java/pojos/CreateUserResponse.java
+++ REST API tests/src/test/java/pojos/CreateUserResponse.java
@@ -1,6 +1,9 @@
 package pojos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import utils.DateDeserializer;
 
 import java.time.LocalDateTime;
 
@@ -9,6 +12,8 @@ public class CreateUserResponse {
     private String name;
     private String job;
     private int id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'hh:mm:ss.SSSZ")
+    @JsonDeserialize(using = DateDeserializer.class)
     private LocalDateTime createdAt;
 
 }
