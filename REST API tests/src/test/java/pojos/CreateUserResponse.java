4901977906db4f8713a589abca786c91e666b6df
dev4lex <dev4lex@gmail.com>
2021-11-10 21:11:27 +0300

    add CreateUserResponse class

---
+++ REST API tests/src/test/java/pojos/CreateUserResponse.java
@@ -0,0 +1,14 @@
+package pojos;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class CreateUserResponse {
+    private String name;
+    private String job;
+    private int id;
+    private LocalDateTime createdAt;
+
+}
